4e50b78730e4fc0e6314b36d1b353e4a7dcacb8a
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-05-05 17:19:12 +0330

    template files of the question

---
+++ troublesome-library/main.go
@@ -0,0 +1,24 @@
+package library
+
+type Library struct {
+}
+
+func NewLibrary(capacity int) *Library {
+	// TODO
+	return nil
+}
+
+func (library *Library) AddBook(name string) string {
+	// TODO
+	return ""
+}
+
+func (library *Library) BorrowBook(bookName, personName string) string {
+	// TODO
+	return ""
+}
+
+func (library *Library) ReturnBook(bookName string) string {
+	// TODO
+	return ""
+}
